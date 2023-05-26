bea6e163af846edf453f4b0c891f9496113ae9e5
amarjyothi98 <amarjyothi526@gmail.com>
2023-05-27 02:31:36 +0530

    done with the error and successful alerts | with firebase error

--- src/pages/AddEdit.jsx
+++ src/pages/AddEdit.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { useParams, useHistory } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import './addEdit.css'
 import app from '../firebase'
 import {toast} from 'react-toastify'
+import database from '../firebase'
 
 const initialState = {
     name: '', 
@@ -17,12 +18,29 @@ export default function AddEdit() {
 
   const { name, email, contact } = state; 
 
-  const handleInputChange = () => {
-    
+  const navigate = useNavigate(); 
+
+  const handleInputChange = (e) => {
+    const {name, value} = e.target; 
+    setState({...state, [name]: value})
   }
 
-  const handleSubmit = () => { 
+  // console.log(state)
 
+  const handleSubmit = (e) => { 
+    e.preventDefault(); 
+    if(!name || !email || !contact) {
+      toast.error("Please provide value in each input field")
+    } else {
+      database.child("contacts").push(state, (err) => {
+        if(err) {
+          toast.error(err); 
+        } else {
+          toast.success("Contact Added Successfully")
+        }
+      });
+      setTimeout(() => navigate("/"), 500); 
+    }
   }
 
   return (
