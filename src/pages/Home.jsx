61b954a9f4178586a79fc93a690581c1d9a635fd
amarjyothi98 <amarjyothi526@gmail.com>
2023-05-30 08:16:44 +0530

    data is storing into the db and data is fetched on to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import fireDb from '../firebase'
+import { Link } from 'react-router-dom'
+import './home.css'
 
 export default function Home() {
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+      fireDb.child('contacts').on('value', (snapshot) => {
+        if(snapshot.val()!==null) {
+          setData({ ...snapshot.val() })
+        } else {
+          setData({}); 
+        }
+      })
+
+      return () => {
+        setData({});
+      };
+  }, [])
+
+  
+
   return (
     <div>
         home.
