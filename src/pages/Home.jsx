900c6808e66fb3c8a04d6ac629eab7dfec1f66da
amarjyothi98 <amarjyothi526@gmail.com>
2023-05-30 09:44:34 +0530

    getting error| unsolved | not getting data from the db

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,8 +24,28 @@ export default function Home() {
   
 
   return (
-    <div>
-        home.
+    <div style={{ marginTop: '100px'}}>
+        <table className='styledTable'>
+          <thead>
+            <tr>
+              <th style={{ textAlign: 'center'}}>No.</th>
+              <th style={{ textAlign: 'center'}}>Name</th>
+              <th style={{ textAlign: 'center'}}>Email</th>
+              <th style={{ textAlign: 'center'}}>Contact</th>
+              <th style={{ textAlign: 'center'}}>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(data).map((id, index) => {
+              <tr key={id}>
+                <th scope='row'>{index + 1}</th>
+                <td>{ data[id].name }</td>
+                <td>{ data[id].email }</td>
+                <td>{ data[id].contact }</td>
+              </tr>
+            })}
+          </tbody>
+        </table>
     </div>
   )
 }
